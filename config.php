f6a1d8bc7e3cdcf5ba4e3186989003d462453b27
kapil-ajency <kapil@ajency.in>
2015-12-01 13:59:07 +0530

    testtt

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $host="localhost";
 $username="root";
 $password="";
-$db_name="amit";
+$db_name="yourdbnamehere";
 mysql_connect("$host", "$username", "$password")or die("cannot connect to server");
 mysql_select_db("$db_name")or die("cannot select db"); 
 ?>
